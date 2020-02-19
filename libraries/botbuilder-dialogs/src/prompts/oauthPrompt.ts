849700945e45fe3923e247cc944dc62be48565f5
swagat mishra <swagatmishra2007@gmail.com>
2020-02-19 01:53:31 -0800

    mostly final changes before pr

--- libraries/botbuilder-dialogs/src/prompts/oauthPrompt.ts
+++ libraries/botbuilder-dialogs/src/prompts/oauthPrompt.ts
@@ -12,14 +12,18 @@ import { PromptOptions, PromptRecognizerResult,  PromptValidator } from './promp
 import { channels } from '../choices/channel';
 import { isSkillClaim } from './skillsHelpers';
 
-// documentation missing
-class TokenExchangeInvokeRequest {
+/**
+ * Request body accepted for a token exchange invoke activity.
+ */
+interface TokenExchangeInvokeRequest {
     id: string;
     connectionName: string;
     token: string;
 }
 
-// documentation missing
+/**
+ * Response body returned for a token exchange invoke activity.
+ */
 class TokenExchangeInvokeResponse {
     id: string;
     connectionName: string;
